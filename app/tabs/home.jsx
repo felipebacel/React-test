6ad508eaae5adeec65fe90c75274e00b5570e59e
Uperlato <rafaeltrindade200@gmail.com>
2024-11-03 15:45:48 -0300

    Debounce Error (Fixed)

--- app/tabs/home.jsx
+++ app/tabs/home.jsx
@@ -14,7 +14,7 @@ import Loading from '../../components/Loading'
 
 
 
-const ios = Platform.OS =='ios';
+const ios = Platform.OS == 'ios';
 const Home = () => {
   const [trending, setTrending] = useState([])
   const [upcoming, setUpcoming] = useState([])
@@ -22,57 +22,57 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
 
 
-  useEffect(()=>{
+  useEffect(() => {
     getTrendingMovies();
     getUpcomingMovies();
     getTopRatedMovies();
-  },[])
+  }, [])
 
-  const getTrendingMovies = async ()=>{
+  const getTrendingMovies = async () => {
     const data = await fetchTrendingMovies();
-    console.log('got trending movies', data);
-    if(data && data.results) setTrending(data.results);
+    // console.log('got trending movies', data);
+    if (data && data.results) setTrending(data.results);
     setLoading(false)
   }
-  const getUpcomingMovies = async ()=>{
+  const getUpcomingMovies = async () => {
     const data = await fetchUpcomingMovies();
-    console.log('got upcoming movies', data);
-    if(data && data.results) setUpcoming(data.results); 
+    // console.log('got upcoming movies', data);
+    if (data && data.results) setUpcoming(data.results);
   }
-  const getTopRatedMovies = async ()=>{
+  const getTopRatedMovies = async () => {
     const data = await fetchTopRatedMovies();
-    console.log('got top rated movies', data);
-    if(data && data.results) setTopRated(data.results);
+    // console.log('got top rated movies', data);
+    if (data && data.results) setTopRated(data.results);
   }
   return (
     <View className='flex-1 bg-primary h-full'>
-      <SafeAreaView className={ios?'-mb-2':'mb-3'}>
+      <SafeAreaView className={ios ? '-mb-2' : 'mb-3'}>
         <View className='flex-row justify-between items-center mx-4'>
-        <Bars3CenterLeftIcon size="30" strokeWidth={2} color='white'/>
-        <Text className='text-white text-3xl font-psemibold'>Filmes</Text>
-        <TouchableOpacity onPress={()=> router.navigate('SearchScreen')}>
-          <MagnifyingGlassIcon size='30' strokeWidth={2} color='white'/>
-        </TouchableOpacity>
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color='white' />
+          <Text className='text-white text-3xl font-psemibold'>Filmes</Text>
+          <TouchableOpacity onPress={() => router.navigate('SearchScreen')}>
+            <MagnifyingGlassIcon size='30' strokeWidth={2} color='white' />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
 
       {
-        loading?(
-          <Loading/>
-        ):(
+        loading ? (
+          <Loading />
+        ) : (
           <ScrollView showsVerticalScrollIndicator={false}
-           contentContainerStyle={{paddingBottom:10}}
-      >
-            {trending.length>0 && <TrendingMovies data={trending}/>}
+            contentContainerStyle={{ paddingBottom: 10 }}
+          >
+            {trending.length > 0 && <TrendingMovies data={trending} />}
 
-            <MovieList title='Lançamentos' data={upcoming}/>
+            <MovieList title='Lançamentos' data={upcoming} />
 
-            <MovieList title='Melhores avaliações' data={topRated}/>
+            <MovieList title='Melhores avaliações' data={topRated} />
 
           </ScrollView>
         )
       }
-      
+
     </View>
   )
 }
